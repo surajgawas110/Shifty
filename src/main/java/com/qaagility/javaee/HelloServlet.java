53b81545946e59278a4cb637fd0038f632139a6b
“surajgawas110” <surajgawas110@gmail.com>
2019-03-14 12:46:50 +0530

    Trial 1

--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -29,9 +29,9 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		String yourName = request.getParameter("yourName");
+		String yourName = request.getParameter("Shifty trial 1");
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.println("<h1>Test " + yourName + " !!</h1>");
 		writer.close();
 	}
 
